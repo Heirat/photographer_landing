06cdaad7b03886a334a69fd418d68c0481caa9b5
Heirat <neocron76@gmail.com>
2021-06-06 14:53:48 +0300

    refactor accordeon script, css and html
    Now old or temp scripts are in old.js.

--- js/script.js
+++ js/script.js
@@ -46,29 +46,8 @@ const toggleMenu = () => {
 
 menuToggleElem.addEventListener('click', toggleMenu)
 
-$(function () {
-  var $speed = 200;
-
-  var $li_opened = 'opened';
-  var $answer_class = '.faq-answer';
-
-  $(document).ready(function () {
-    $('.faq-question').on('click', function () {
-      $ul = $(this).closest('ul');
-      $answer = $(this).closest('li').find($answer_class);
-
-      if (!$(this).closest('li').hasClass($li_opened)) {
-
-        $ul.find('li').each(function () {
-          if ($(this).hasClass($li_opened))
-            $(this).removeClass($li_opened).find($answer_class).slideUp($speed);
-        });
-      }
-
-      $answer
-        .slideToggle($speed)
-        .closest('li')
-        .toggleClass($li_opened);
-    });
+$(document).ready(function(){
+  $(".faq-question").on("click", function() {
+    $(this).parent().toggleClass('opened');    
   });
 });
