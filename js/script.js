c14f3eebbeee6f2e0634765bd100336a5258e5c6
Heirat <neocron76@gmail.com>
2021-06-23 15:26:49 +0300

    fix: replace JQuery code with JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 new WOW().init();
 
-// Menu (begin) 
+// Menu (begin)
 const headerOuterElem = document.querySelector('.header-outer');
 const menuElem = document.querySelector('.menu');
 //const menuBtnElem = document.querySelector('.menu__btn');
@@ -10,61 +10,64 @@ const headerLogoElem = document.querySelector('.logo');
 const overlayElem = document.querySelector('.overlay');
 
 const noScrollState = () => {
-  // Шапка не прокручена
-  headerOuterElem.classList.remove('scrolled');
-  // Скрыть бургер-кнопку
-  menuElem.classList.add('d-none');
-  closeMenu();
+	// Шапка не прокручена
+	headerOuterElem.classList.remove('scrolled');
+	// Скрыть бургер-кнопку
+	menuElem.classList.add('d-none');
+	closeMenu();
 };
 
 const scrollState = () => {
-  // Шапка прокручена
-  headerOuterElem.classList.add('scrolled');
-  // Показать бургер-кнопку
-  menuElem.classList.remove('d-none');
+	// Шапка прокручена
+	headerOuterElem.classList.add('scrolled');
+	// Показать бургер-кнопку
+	menuElem.classList.remove('d-none');
 };
 
 window.onscroll = () => {
-  let scrollPos = window.pageYOffset;
-  if (scrollPos < 100) {
-    noScrollState();
-  } else {
-    scrollState();
-  }
+	let scrollPos = window.pageYOffset;
+	if (scrollPos < 100) {
+		noScrollState();
+	} else {
+		scrollState();
+	}
 };
 
 const toggleMenu = () => {
-  // Переключить состояние шапки на откртое меню
-  headerOuterElem.classList.toggle('menu-opened');
-  // Переключить лого
-  headerLogoElem.classList.toggle('invisible');
-  // Переключить наложение
-  overlayElem.classList.toggle('invisible');
+	// Переключить состояние шапки на откртое меню
+	headerOuterElem.classList.toggle('menu-opened');
+	// Переключить лого
+	headerLogoElem.classList.toggle('invisible');
+	// Переключить наложение
+	overlayElem.classList.toggle('invisible');
 };
 
 const closeMenu = () => {
-  headerOuterElem.classList.remove('menu-opened');
-  headerLogoElem.classList.remove('invisible');
-  overlayElem.classList.add('invisible');
-  // Закрыть список меню
-  menuToggleElem.checked = false;
-}
+	headerOuterElem.classList.remove('menu-opened');
+	headerLogoElem.classList.remove('invisible');
+	overlayElem.classList.add('invisible');
+	// Закрыть список меню
+	menuToggleElem.checked = false;
+};
 
 menuToggleElem.addEventListener('click', toggleMenu);
 menuElem.addEventListener('click', (event) => {
-  const target = event.target;
-  console.log(target);
-  if (target.classList.contains('overlay') ||
-    target.classList.contains('menu__item')) {
-    closeMenu();
-  }
+	const target = event.target;
+	console.log(target);
+	if (
+		target.classList.contains('overlay') ||
+		target.classList.contains('menu__item')
+	) {
+		closeMenu();
+	}
 });
 // Menu (end)
 
 // Faq (begin)
-$(document).ready(function () {
-  $(".faq-question").on("click", function () {
-    $(this).parent().toggleClass('opened');
-  });
+const faqQuestion = document.querySelectorAll('.faq-question');
+faqQuestion.forEach((question) => {
+	question.addEventListener('click', () => {
+		question.closest('.faq-questions-item').toggleClass('opened');
+	});
 });
 // Faq (end)
